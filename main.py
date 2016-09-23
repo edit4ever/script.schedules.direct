4f4fcd625ea52d7d49a0aedb58b2c8846135b0f9
primaeval <primaeval@users.noreply.github.com>
2016-09-23 17:37:39 +0200

    export xmltv

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import hashlib
 import datetime,time
 from utils import *
 import sqlite3
+import cgi
 
 plugin = Plugin()
 
@@ -138,41 +139,36 @@ def add_channels():
     name = lineups[sel]
     sel_lineup = [x for x in lineup_list if x["name"] == name]
     if not len(sel_lineup):
-        return        
+        return
     sel_lineup = sel_lineup[0]
     lineup = sel_lineup['lineup']
     station_list = sorted(sd.get_stations(lineup=lineup), key=lambda s: s.title.lower())
-    xbmc.log(repr(station_list))
+    #xbmc.log(repr(station_list))
     if len(station_list) == 0:
         return
     stations = []
     for channel in station_list:
-        xbmc.log(repr(channel))
+        #xbmc.log(repr(channel))
         stations.append(channel.title)
     stations = sorted(stations, key=lambda s: s.lower())
-    xbmc.log(repr(stations))
+    #xbmc.log(repr(stations))
     sel = xbmcgui.Dialog().multiselect('Select lineup', stations)
-    xbmc.log(repr(sel))
+    #xbmc.log(repr(sel))
     if not sel:
         return
-    
+
     path = xbmc.translatePath("special://profile/addon_data/script.schedules.direct/sd.db")
     conn = sqlite3.connect(path)
     conn.row_factory = sqlite3.Row
-    c = conn.cursor()    
+    c = conn.cursor()
     for s in sel:
         channel = station_list[s]
         c.execute("INSERT OR REPLACE INTO channels(lineup,id,title,logo) VALUES(?,?,?,?)",
-                      [lineup,channel.id,channel.title,channel.logo])    
-    conn.commit()                            
+                      [lineup,channel.id,channel.title,channel.logo])
+    conn.commit()
     c.close()
-    #if sel < 0:
-    #    return
-    #name = lineups[sel]
-    #sel_station = [x for x in station_list if x["name"] == name]
-    #if not len(sel_station):
-    #    return   
-        
+
+
 def to_local(time_str):
     # format: 2016-08-21T00:45:00Z
     try:
@@ -187,20 +183,20 @@ def to_local(time_str):
     t_local = utc + td_local
     return t_local
 
-@plugin.route('/export_xmltv')
-def export_xmltv():
+@plugin.route('/import_schedule')
+def import_schedule():
     progress_callback = None
     user = plugin.get_setting('sd.username')
     passw = plugin.get_setting('sd.password')
     passw = hashlib.sha1(passw.encode('utf-8')).hexdigest()
     sd = SdAPI(user=user, passw=passw)
 
-    ch_list = []
-    station_ids = []
-    for ch in ch_list:
-        station_ids.append(ch.id)
-    station_ids = ["34135"]
-    # make sure date is in UTC!
+    path = xbmc.translatePath("special://profile/addon_data/script.schedules.direct/sd.db")
+    conn = sqlite3.connect(path)
+    conn.row_factory = lambda cursor, row: row[0]
+    c = conn.cursor()
+    station_ids = c.execute('SELECT id FROM channels').fetchall()
+
     date_local = datetime.datetime.now()
     is_dst = time.daylight and time.localtime().tm_isdst > 0
     utc_offset = time.altzone if is_dst else time.timezone
@@ -209,26 +205,60 @@ def export_xmltv():
     xbmc.log("[%s] Local date '%s' converted to UTC '%s'" %
              (xbmcaddon.Addon().getAddonInfo('id'), str(date_local), str(date)), xbmc.LOGDEBUG)
 
-    # [{'station_id': station_id, 'p_id': p_id, 'start': start,
-    #   'dur': dur, 'title': 'abc', 'desc': 'abc', 'logo': ''}, ... ]
+
     elements_parsed = 0
     schedules = sd.get_schedules(station_ids, date, progress_callback)
+    conn.row_factory = sqlite3.Row
     for prg in schedules:
         start = to_local(prg['start'])
         end = start + datetime.timedelta(seconds=int(prg['dur']))
         result = Program(prg['station_id'], prg['title'], start, end, prg['desc'],
                          imageSmall=prg['logo'])
-        xbmc.log(repr(result))
+        #xbmc.log(repr(result))
         elements_parsed += 1
-        #if result:
-        #    if progress_callback and elements_parsed % 100 == 0:
-        #        if not progress_callback(100.0 / len(schedules) * elements_parsed):
-        #            raise SourceUpdateCanceledException()
-        #    yield result    
+        c.execute("INSERT OR REPLACE INTO programs(station_id,p_id,title,logo,start,dur,desc) VALUES(?,?,?,?,?,?,?)",
+                      [prg['station_id'], prg['p_id'], prg['title'], prg['logo'], prg['start'], prg['dur'], prg['desc']])
 
+    conn.commit()
+    c.close()
+
+@plugin.route('/export_xmltv')
+def export_xmltv():
+    path = xbmc.translatePath("special://profile/addon_data/script.schedules.direct/sd.db")
+    conn = sqlite3.connect(path)
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    channels = c.execute('SELECT * FROM channels').fetchall()
+
+    path = "special://profile/addon_data/script.schedules.direct/xmltv.xml"
+    f = xbmcvfs.File(path,"wb")
+    f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    f.write('<tv generator-info-name="script.schedules.direct" generator-info-url="TODO">\n')
+
+    for channel in channels:
+        f.write('<channel id="%s">\n' % channel["id"].encode("utf8"))
+        f.write('  <display-name lang="en">%s</display-name>\n' % channel["title"].encode("utf8"))
+        f.write('  <icon src="%s" />\n' % channel["logo"].encode("utf8"))
+        f.write('</channel>\n')
+
+    #TODO just a few
+    programs = c.execute('SELECT * FROM programs').fetchall()
+    for program in programs:
+        start = time.strptime(program["start"], "%Y-%m-%dT%H:%M:%SZ")
+        start = datetime.datetime.fromtimestamp(time.mktime(start))
+        end = start + datetime.timedelta(0,int(program["dur"]))
+        #TODO timezone offset
+        start = start.strftime("%Y%m%d%H%M%S +0000")
+        end = end.strftime("%Y%m%d%H%M%S +0000")
+        f.write('<programme start="%s" stop="%s" channel="%s">\n' % (start.encode("utf8"),end.encode("utf8"),program["station_id"].encode("utf8")))
+        f.write('<title lang="en">%s</title>\n' % cgi.escape(program["title"]).encode("utf8"))
+        f.write('<desc lang="en">%s</desc>\n' % cgi.escape(program["desc"]).encode("utf8"))
+        f.write('</programme>\n')
+
+    f.write("</tv>\n")
 
 @plugin.route('/create_database')
-def create_database():     
+def create_database():
     path = xbmc.translatePath("special://profile/addon_data/script.schedules.direct/sd.db")
     conn = sqlite3.connect(path)
     conn.row_factory = sqlite3.Row
@@ -236,17 +266,11 @@ def create_database():
     c.execute('CREATE TABLE IF NOT EXISTS channels(lineup TEXT, id TEXT, title TEXT, logo TEXT, stream_url TEXT, visible BOOLEAN, PRIMARY KEY (lineup,id))')
     c.execute('CREATE TABLE IF NOT EXISTS programs(station_id TEXT, p_id TEXT, title TEXT, logo TEXT, start TEXT, dur TEXT, desc TEXT, PRIMARY KEY (station_id,p_id))')
     c.close()
-   
-        
+
+
 @plugin.route('/')
 def index():
     items = []
-    items.append(
-    {
-        'label': 'Create Database',
-        'path': plugin.url_for('create_database'),
-        'thumbnail':get_icon_path('settings'),
-    })    
     items.append(
     {
         'label': 'Add TV Provider',
@@ -267,6 +291,12 @@ def index():
     })
     items.append(
     {
+        'label': 'Import Schedule',
+        'path': plugin.url_for('import_schedule'),
+        'thumbnail':get_icon_path('settings'),
+    })
+    items.append(
+    {
         'label': 'Export xmltv',
         'path': plugin.url_for('export_xmltv'),
         'thumbnail':get_icon_path('settings'),
@@ -275,4 +305,5 @@ def index():
 
 
 if __name__ == '__main__':
+    create_database()
     plugin.run()
